325bbe86e02553695fe17f78646a52b04ce9d85d
Nam Nguyen <NamNguyen@Nams-MacBook-Pro.local>
2017-03-28 13:55:06 -0400

    Initial commit. ExpandableListView created

---
+++ app/src/main/java/com/example/namnguyen/taskorganizer/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.namnguyen.taskorganizer;
+
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.floatingActionButton);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+}
