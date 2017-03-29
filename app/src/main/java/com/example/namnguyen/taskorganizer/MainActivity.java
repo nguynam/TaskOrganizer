5ed00f58864019244aa85157ef52aef6b8657e64
Nam Nguyen <NamNguyen@Nams-MBP.hsd1.mi.comcast.net>
2017-03-28 20:21:03 -0400

    Added one item to listview

--- app/src/main/java/com/example/namnguyen/taskorganizer/MainActivity.java
+++ app/src/main/java/com/example/namnguyen/taskorganizer/MainActivity.java
@@ -5,13 +5,30 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ExpandableListView;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    ExpandableListView expandableListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        expandableListView = (ExpandableListView) findViewById(R.id.listView);
+        List<String> headings = new ArrayList<String>();
+        List<String> childItems = new ArrayList<String>();
+        HashMap<String, List<String>> childList = new HashMap<String, List<String>>();
+        String heading = "Buy Food";
+        String child = "Due: 3/27/2017";
+        headings.add(heading);
+        childItems.add(child);
+        childList.put(headings.get(0),childItems);
+        MyAdapter myAdapter = new MyAdapter(this, headings, childList);
+        expandableListView.setAdapter(myAdapter);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.floatingActionButton);
         fab.setOnClickListener(new View.OnClickListener() {
---
+++ app/src/main/java/com/example/namnguyen/taskorganizer/MyAdapter.java
@@ -0,0 +1,95 @@
+package com.example.namnguyen.taskorganizer;
+
+import android.content.Context;
+import android.graphics.Typeface;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseExpandableListAdapter;
+import android.widget.TextView;
+
+import java.util.HashMap;
+import java.util.List;
+
+/**
+ * Created by NamNguyen on 3/28/17.
+ */
+
+
+public class MyAdapter extends BaseExpandableListAdapter {
+    private List<String> header;
+    private HashMap<String,List<String>> child_items;
+    private Context ctx;
+
+    MyAdapter(Context ctx, List<String> header, HashMap<String,List<String>> child_items){
+        this.ctx = ctx;
+        this.header = header;
+        this.child_items = child_items;
+    }
+
+    @Override
+    public int getGroupCount() {
+        return header.size();
+    }
+
+    @Override
+    public int getChildrenCount(int groupPosition) {
+        return child_items.get(header.get(groupPosition)).size();
+    }
+
+    @Override
+    public Object getGroup(int groupPosition) {
+        return header.get(groupPosition);
+    }
+
+    @Override
+    public Object getChild(int groupPosition, int childPosition) {
+        return child_items.get(header.get(groupPosition)).get(childPosition);
+    }
+
+    @Override
+    public long getGroupId(int groupPosition) {
+        return groupPosition;
+    }
+
+    @Override
+    public long getChildId(int groupPosition, int childPosition) {
+        return childPosition;
+    }
+
+    @Override
+    public boolean hasStableIds() {
+        return false;
+    }
+
+    @Override
+    public View getGroupView(int groupPosition, boolean isExpanded, View convertView, ViewGroup parent) {
+        String header = (String)this.getGroup(groupPosition);
+        if(convertView == null){
+            LayoutInflater layoutInflater = (LayoutInflater) this.ctx.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = layoutInflater.inflate(R.layout.parent_layout,null);
+        }
+        TextView textView = (TextView) convertView.findViewById(R.id.header);
+        textView.setTypeface(null, Typeface.BOLD);
+        textView.setText(header);
+
+        return convertView;
+    }
+
+    @Override
+    public View getChildView(int groupPosition, int childPosition, boolean isLastChild, View convertView, ViewGroup parent) {
+        String title = (String)this.getChild(groupPosition, childPosition);
+        if(convertView == null){
+            LayoutInflater layoutInflater = (LayoutInflater) this.ctx.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = layoutInflater.inflate(R.layout.child_layout,null);
+        }
+        TextView textView = (TextView) convertView.findViewById(R.id.childItem);
+        textView.setText(title);
+        return convertView;
+    }
+
+    @Override
+    public boolean isChildSelectable(int groupPosition, int childPosition) {
+        return true;
+    }
+}
