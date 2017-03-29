8f9de2682ffd0d43ad476382d7d4ac8c24a7d03d
Nam Nguyen <NamNguyen@Nams-MBP.hsd1.mi.comcast.net>
2017-03-28 20:46:34 -0400

    Minor visual changes

--- app/src/main/java/com/example/namnguyen/taskorganizer/MainActivity.java
+++ app/src/main/java/com/example/namnguyen/taskorganizer/MainActivity.java
@@ -22,13 +22,14 @@ public class MainActivity extends AppCompatActivity {
         List<String> headings = new ArrayList<String>();
         List<String> childItems = new ArrayList<String>();
         HashMap<String, List<String>> childList = new HashMap<String, List<String>>();
-        String heading = "Buy Food";
-        String child = "Due: 3/27/2017";
+        String heading = "Buy Food \n" + "Due:3/27/2017";
+        String child = "Description: Buy Chicken, Ham, Bacon";
         headings.add(heading);
         childItems.add(child);
         childList.put(headings.get(0),childItems);
         MyAdapter myAdapter = new MyAdapter(this, headings, childList);
         expandableListView.setAdapter(myAdapter);
+        expandableListView.setChildDivider(getResources().getDrawable(R.color.transparent));
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.floatingActionButton);
         fab.setOnClickListener(new View.OnClickListener() {
