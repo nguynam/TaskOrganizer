c7d9d0a0825dbf70d8db243a139d1de9b7e9c636
Nam Nguyen <NamNguyen@Nams-MBP.hsd1.mi.comcast.net>
2017-04-03 18:02:32 -0400

    Google Place Picker working

--- app/src/main/java/com/example/namnguyen/taskorganizer/MainActivity.java
+++ app/src/main/java/com/example/namnguyen/taskorganizer/MainActivity.java
@@ -10,6 +10,7 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.SystemClock;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AlertDialog;
@@ -24,13 +25,23 @@ import android.widget.EditText;
 import android.widget.ExpandableListView;
 import android.widget.TimePicker;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
+import com.google.android.gms.common.GooglePlayServicesRepairableException;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.Places;
+import com.google.android.gms.location.places.ui.PlaceAutocomplete;
+
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Locale;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener {
+    int PLACE_AUTOCOMPLETE_REQUEST_CODE = 1;
+
     private int currentYear;
     private int currentMonth;
     private int currentDay;
@@ -46,19 +57,46 @@ public class MainActivity extends AppCompatActivity {
     private String date;
     private String time;
     private String address;
+    private boolean changingData = false;
 
     DatePickerDialog datePickerDialog;
     TimePickerDialog timePickerDialog;
     Dialog timeSpanDialog;
 
-
     private ExpandableListView expandableListView;
     final List<String> headings = new ArrayList<>();
     HashMap<String, List<String>> childList = new HashMap<>();
+
+    final List<String> thisHeadings = new ArrayList<>();
+    HashMap<String, List<String>> thisChildList = new HashMap<>();
+
     final MyAdapter myAdapter = new MyAdapter(this, headings, childList);
     String heading;
     String child1;
     String child2 = "\nAdd Location?";
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(requestCode == PLACE_AUTOCOMPLETE_REQUEST_CODE){
+            if(resultCode == RESULT_OK){
+                Place pl = PlaceAutocomplete.getPlace(this, data);
+                address = "\nAddress:" + "\n" + pl.getAddress().toString();
+                if(changingData){
+                    myAdapter.replaceChildItem(task, address, 1);
+                    myAdapter.notifyDataSetChanged();
+                }
+                else{
+                    List<String> childItems = new ArrayList<>();
+                    childItems.add(child1);
+                    childItems.add(address);
+
+                    myAdapter.addHeader(heading, -1);
+                    myAdapter.addChild(heading, childItems);
+                    myAdapter.notifyDataSetChanged();
+                }
+            }
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -67,6 +105,12 @@ public class MainActivity extends AppCompatActivity {
         expandableListView = (ExpandableListView) findViewById(R.id.listView);
         expandableListView.setAdapter(myAdapter);
         expandableListView.setChildDivider(getResources().getDrawable(R.color.transparent));
+
+        GoogleApiClient apiClient = new GoogleApiClient.Builder(this)
+                .addApi(Places.GEO_DATA_API)
+                .addApi(Places.PLACE_DETECTION_API)
+                .enableAutoManage(this, this)
+                .build();
 
         //Creates alert dialog for deleting a task
         AlertDialog.Builder builder1 = new AlertDialog.Builder(this);
@@ -105,10 +149,14 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        //Edit child items
         expandableListView.setOnChildClickListener(new ExpandableListView.OnChildClickListener() {
             @Override
             public boolean onChildClick(ExpandableListView parent, View v, final int groupPosition, final int childPosition, long id) {
-                if(childPosition == 0){
+                Snackbar.make(v, "Position: " + childPosition, Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+                if (childPosition == 0) {
+                    //Show title/description dialog
                     final Dialog dialog = new Dialog(MainActivity.this);
                     dialog.setContentView(R.layout.title_description_dialog);
                     Window window = dialog.getWindow();
@@ -120,18 +168,21 @@ public class MainActivity extends AppCompatActivity {
                     Button okButton = (Button) dialog.findViewById(R.id.ok);
                     Button cancelButton = (Button) dialog.findViewById(R.id.cancel);
 
+                    //Retrieve original header and description
                     String originalHeader = myAdapter.getHeader(groupPosition);
                     String originalDescription = myAdapter.getChild(originalHeader, childPosition);
+                    final String headerSplit[] = originalHeader.split("\n");
+                    final String descriptionSplit[] = originalDescription.split("\n");
 
-                    title.setText(originalHeader);
-                    description.setText(originalDescription);
+                    title.setText(headerSplit[0]);
+                    description.setText(descriptionSplit[1]);
 
                     okButton.setOnClickListener(new View.OnClickListener() {
                         @Override
                         public void onClick(View v) {
-                            //Retrieves text from dialog
-                            String task = title.getText().toString();
-                            String taskDescription = description.getText().toString();
+                            //Retrieves text from dialog and update lists
+                            String task = title.getText().toString() + "\n" + headerSplit[1];
+                            String taskDescription = descriptionSplit[0] + "\n" + description.getText().toString();
                             myAdapter.addHeader(task, groupPosition);
                             myAdapter.replaceChildItem(task, taskDescription, childPosition);
                             myAdapter.notifyDataSetChanged();
@@ -145,6 +196,20 @@ public class MainActivity extends AppCompatActivity {
                             dialog.cancel();
                         }
                     });
+                }
+                if (childPosition == 1) {
+                    changingData = true;
+                    try {
+                        Intent intent =
+                                new PlaceAutocomplete.IntentBuilder(PlaceAutocomplete.MODE_OVERLAY)
+                                        .build(MainActivity.this);
+                        startActivityForResult(intent, PLACE_AUTOCOMPLETE_REQUEST_CODE);
+                        task = myAdapter.getHeader(groupPosition);
+                    } catch (GooglePlayServicesRepairableException e) {
+                        e.printStackTrace();
+                    } catch (GooglePlayServicesNotAvailableException e) {
+                        e.printStackTrace();
+                    }
                 }
                 return false;
             }
@@ -173,8 +238,13 @@ public class MainActivity extends AppCompatActivity {
                     @Override
                     public void onClick(View v) {
                         //Retrieves text from dialog
+                        List<String> temp = new ArrayList<String>();
                         task = title.getText().toString();
+                        thisHeadings.add(task);
                         taskDescription = description.getText().toString();
+                        temp.add(taskDescription);
+                        thisChildList.put(task, temp);
+
                         child1 = "Description: \n" + taskDescription;
                         dialog.dismiss();
 
@@ -237,7 +307,7 @@ public class MainActivity extends AppCompatActivity {
             builder.setTitle("Set Reminder");
             builder.setCancelable(true);
             builder.setPositiveButton("Okay", timeSpanListener);
-            builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener(){
+            builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
                 @Override
                 public void onClick(DialogInterface dialogInterface, int i) {
                     timeSpanDialog.dismiss();
@@ -258,26 +328,57 @@ public class MainActivity extends AppCompatActivity {
     private DialogInterface.OnClickListener timeSpanListener = new DialogInterface.OnClickListener() {
         @Override
         public void onClick(DialogInterface dialogInterface, int i) {
-            if(currentDay == selectedDay && currentMonth == selectedMonth){
+            if (currentDay == selectedDay && currentMonth == selectedMonth) {
                 date = "\nToday";
-            }
-            else if(selectedDay == currentDay+1 && currentMonth == selectedMonth || (currentDay == 30 || currentDay == 31) && selectedDay == 1){
+            } else if (selectedDay == currentDay + 1 && currentMonth == selectedMonth || (currentDay == 30 || currentDay == 31) && selectedDay == 1) {
                 date = "\nTomorrow";
-            }
-            else{
+            } else {
                 date = "\n" + (selectedMonth + 1) + "/" + selectedDay + "/" + selectedYear;
             }
 
             heading = task + date + " at " + time;
 
-            List<String> childItems = new ArrayList<>();
-            childItems.add(child1);
-            childItems.add(child2);
+            final Dialog dialog = new Dialog(MainActivity.this);
+            dialog.setContentView(R.layout.google_places_picker);
+            Window window = dialog.getWindow();
+            window.setLayout(700, 500);
+            dialog.show();
+            changingData = false;
 
-            myAdapter.addHeader(heading, -1);
-            myAdapter.addChild(heading, childItems);
-            myAdapter.notifyDataSetChanged();
-            scheduleNotification("Reminder: " + heading,heading + " is coming up soon", 500);
+            Button okButton = (Button) dialog.findViewById(R.id.locationYes);
+            Button cancelButton = (Button) dialog.findViewById(R.id.locationNo);
+
+            okButton.setOnClickListener(new View.OnClickListener(){
+                @Override
+                public void onClick(View v) {
+                    try {
+                        Intent intent =
+                                new PlaceAutocomplete.IntentBuilder(PlaceAutocomplete.MODE_OVERLAY)
+                                        .build(MainActivity.this);
+                        startActivityForResult(intent, PLACE_AUTOCOMPLETE_REQUEST_CODE);
+                    } catch (GooglePlayServicesRepairableException e) {
+                        e.printStackTrace();
+                    } catch (GooglePlayServicesNotAvailableException e) {
+                        e.printStackTrace();
+                    }
+                    dialog.dismiss();
+                }
+            });
+
+            cancelButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    List<String> childItems = new ArrayList<>();
+                    childItems.add(child1);
+                    childItems.add(child2);
+
+                    myAdapter.addHeader(heading, -1);
+                    myAdapter.addChild(heading, childItems);
+                    myAdapter.notifyDataSetChanged();
+                    dialog.dismiss();
+                }
+            });
+            scheduleNotification("Reminder: " + heading, heading + " is coming up soon", 500);
         }
     };
     private DialogInterface.OnClickListener dateCancelListener = new DialogInterface.OnClickListener() {
@@ -296,6 +397,7 @@ public class MainActivity extends AppCompatActivity {
             myAdapter.notifyDataSetChanged();
         }
     };
+
     private void scheduleNotification(String title, String message, int delay) {
         //Build notification
         Notification.Builder builder = new Notification.Builder(this);
@@ -313,7 +415,12 @@ public class MainActivity extends AppCompatActivity {
         PendingIntent pendingIntent = PendingIntent.getBroadcast(this, 0, notificationIntent, PendingIntent.FLAG_UPDATE_CURRENT);
         notification.contentIntent = pendingIntent;
         long futureInMillis = SystemClock.elapsedRealtime() + delay;
-        AlarmManager alarmManager = (AlarmManager)getSystemService(Context.ALARM_SERVICE);
+        AlarmManager alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
         alarmManager.set(AlarmManager.ELAPSED_REALTIME_WAKEUP, futureInMillis, pendingIntent);
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        return;
     }
 }
