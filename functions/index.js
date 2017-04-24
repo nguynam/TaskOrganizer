cdbba4abba1a4cd2f4c1313735b1f1578c05498c
Josh Getter <getterj@mail.gvsu.edu>
2017-04-24 11:36:27 -0400

    Date comparison working

--- functions/index.js
+++ functions/index.js
@@ -1,47 +1,40 @@
-require("@google-cloud/debug-agent").start();
 var functions = require("firebase-functions");
 const admin = require("firebase-admin");
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+var moment = require("moment-timezone");
 admin.initializeApp(functions.config().firebase);
-/*exports.sendNotification = functions.database.ref("/users/{userId}/{userTasks}")
-        .onWrite(event => {
- 
-        // Grab the current value of what was written to the Realtime Database.
-        var eventSnapshot = event.data;
-        var userId = event.params.userId;
-        console.log("User: " + userId);
-        });*/
 exports.minute_job = functions.https.onRequest((req, res) => {
   console.log("Cron has been called, checking for notifications to send");
   var usersRef = admin.database().ref("/users");
-  console.log("UsersRef: " + usersRef);
   usersRef.once("value", function (data) {
     dataVal = data.val();
-    console.log(dataVal);
-    /*var user = dataVal[Object.keys(dataVal)[0]];
-    console.log(user);
-    var userTasks = user.userTasks;
-    console.log(userTasks);
-    var children = userTasks.children;
-    console.log(children);
-    var json = JSON.parse(unescape(children));
-    console.log(json[Object.keys(json)[0]]);*/
     for(var user in dataVal){
-      console.log(user);
       var userTasks = dataVal[user].userTasks;
-      console.log(userTasks);
       var children = userTasks.children;
-      console.log(children);
       var json = JSON.parse(unescape(children));
       for(var task in json){
-        console.log(json[task][3]);
+        var reminderText = json[task][3];
+        var dueDateText = json[task][1];
+        console.log(reminderText);
+        if(reminderText.includes("Before")){
+            reminderText = reminderText.substring(reminderText.indexOf(":")+2);
+            var dueMoment = moment.tz(dueDateText,"M/DD/YYYY hh:mm A","America/New_York");
+            var currentTime = moment().tz("America/New_York");
+            var reminderValue = reminderText.substring(0,1);
+            var reminderUnit = reminderText.substring(2,reminderText.indexOf("B")-1).toLowerCase();
+            console.log("ReminderUnit: " + reminderUnit + " Reminder Value: " + reminderValue);
+            var reminderMoment = moment.tz(dueMoment,"America/New_York").subtract(reminderValue, reminderUnit);
+            if(reminderMoment.isSame(currentTime,"minute")){
+              //Same time
+              console.log("Same Time");
+            }else{
+              //Time differs
+              console.log("Time Differs");
+            }
+            console.log(currentTime);
+            console.log(reminderMoment);
+            console.log(dueMoment);
+        }
       }
-      //console.log(json[Object.keys(json)[3]]);
     }
   });
   res.send("Check completed");
