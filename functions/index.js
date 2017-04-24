ea9981f1c83cd7076a1be70989bd449b7b6de13a
Josh Getter <getterj@mail.gvsu.edu>
2017-04-24 09:37:52 -0400

    Can finally read from database

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,6 @@
-require('@google-cloud/debug-agent').start({ allowExpressions: true });
-var functions = require('firebase-functions');
-const admin = require('firebase-admin');
+require("@google-cloud/debug-agent").start();
+var functions = require("firebase-functions");
+const admin = require("firebase-admin");
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
@@ -8,7 +8,7 @@ const admin = require('firebase-admin');
 //  response.send("Hello from Firebase!");
 // });
 admin.initializeApp(functions.config().firebase);
-/*exports.sendNotification = functions.database.ref('/users/{userId}/{userTasks}')
+/*exports.sendNotification = functions.database.ref("/users/{userId}/{userTasks}")
         .onWrite(event => {
  
         // Grab the current value of what was written to the Realtime Database.
@@ -18,12 +18,32 @@ admin.initializeApp(functions.config().firebase);
         });*/
 exports.minute_job = functions.https.onRequest((req, res) => {
   console.log("Cron has been called, checking for notifications to send");
-  var usersRef = admin.database().ref('/users');
-  console.log('UsersRef: ' + usersRef);
-  usersRef.once('value',function(snapshot){
-    console.log('snapshot: ' + snapshot);
-    console.log('snapshotString: ' + JSON.stringify(snapshot));
+  var usersRef = admin.database().ref("/users");
+  console.log("UsersRef: " + usersRef);
+  usersRef.once("value", function (data) {
+    dataVal = data.val();
+    console.log(dataVal);
+    /*var user = dataVal[Object.keys(dataVal)[0]];
+    console.log(user);
+    var userTasks = user.userTasks;
+    console.log(userTasks);
+    var children = userTasks.children;
+    console.log(children);
+    var json = JSON.parse(unescape(children));
+    console.log(json[Object.keys(json)[0]]);*/
+    for(var user in dataVal){
+      console.log(user);
+      var userTasks = dataVal[user].userTasks;
+      console.log(userTasks);
+      var children = userTasks.children;
+      console.log(children);
+      var json = JSON.parse(unescape(children));
+      for(var task in json){
+        console.log(json[task][3]);
+      }
+      //console.log(json[Object.keys(json)[3]]);
+    }
   });
-  res.send('Check completed');
+  res.send("Check completed");
   return;
 });
